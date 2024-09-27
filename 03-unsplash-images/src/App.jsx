1198b149ecdbc265775b3150fcf57e8009886f18
hamza-yameen <hamzayameen3000@gmail.com>
2024-09-27 16:44:26 +0500

    add scrolling in unsplash project

--- 03-unsplash-images/src/App.jsx
+++ 03-unsplash-images/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 import Form from './components/Form'
 import Photo from './components/Photo'
+import Loader from './components/Loader'
 
 const clientID = `?client_id=${process.env.REACT_APP_UNSPLASH_API_CLIENT_ID}`
 const mainUrl = process.env.REACT_APP_MAIN_URL
 const searchUrl = process.env.REACT_APP_SEARCH_URL
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('random')
-  const [page, setPage] = useState(3)
+  const [searchQuery, setSearchQuery] = useState()
+  const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(false)
   const [unsplashImages, setUnsplashImages] = useState([])
+  const [newImages, setNewImages] = useState(false)
 
   const fetchImages = async (newPage = null) => {
     setLoading(true)
@@ -28,16 +30,21 @@ function App() {
     try {
       const response = await fetch(url)
       const responseData = await response.json()
+
       setUnsplashImages((oldImages) => {
-        if (searchQuery && newPage) {
+        if (searchQuery && page === 1) {
           return responseData.results
-        } else {
+        } else if (searchQuery) {
           return [...oldImages, ...responseData.results]
+        } else {
+          return [...oldImages, ...responseData]
         }
       })
       setLoading(false)
+      setNewImages(false)
     } catch (error) {
       setLoading(false)
+      setNewImages(false)
     }
   }
 
@@ -49,9 +56,25 @@ function App() {
     }
   }
 
+  const onScrollEventHandler = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScrollEventHandler)
+    return () => window.removeEventListener('scroll', onScrollEventHandler)
+  }, [])
+
+  useEffect(() => {
+    if (loading && !newImages) return
+    setPage((prePage) => prePage + 1)
+  }, [newImages])
+
   useEffect(() => {
     fetchImages()
-  }, [])
+  }, [page])
 
   return (
     <div className="bg-home-custom-gradient min-h-screen w-screen">
@@ -68,6 +91,7 @@ function App() {
             ))}
         </div>
       </div>
+      {loading && <Loader />}
     </div>
   )
 }
